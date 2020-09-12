4026d6e827dcc5bb74fc6b5b5db1a004872186c0
Arsus1 <kuzmin.ars@yandex.ru>
2020-09-12 23:35:09 +0300

    First version *пока кое-как работает

---
+++ Js/front_script.js
@@ -0,0 +1,81 @@
+let   X = document.getElementById('x-value');
+let r_values = document.getElementById('r-value');
+let y_values = document.getElementsByName('y-value');
+let Y = null;
+let R;
+function checkX() {
+    if (X.value.trim() === "") {
+        return false
+    }else {
+        if (isFinite(X.value)) {
+            if (X.value.trim() >= -5 && X.value <= 5) {
+                return true
+            } else {
+                X.setCustomValidity("Must be (-5;5)");
+                return false
+            }
+        } else {
+            X.setCustomValidity("Must be number");
+            return false
+        }
+    }
+}
+
+function checkY() {
+    for (let i = 0; i < y_values.length; i++) {
+        if (y_values[i].checked) {
+            Y = y_values[i].value;
+            break;
+        }
+
+    }
+    if (Y == null){
+
+        alert("Choose one Y value");
+        return false
+
+    } else {return true}
+}
+
+
+const submit = function(ev) {
+    if (!(checkY())) return
+    if (!(checkX())) return
+    ev.preventDefault();
+    R=r_values.options[r_values.selectedIndex].value
+
+    const data = new FormData();
+
+    data.append('x_val', X.value);
+    data.append("y_val", Y);
+    data.append('r_val', R);
+
+
+
+    fetch('Php/function.php?' + new URLSearchParams({
+        x_val:X.value,
+        y_val:Y,
+        r_val:R
+    }))
+
+        .then(res => res.text())
+        .then(table => document.querySelector('#res').innerHTML = table);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('sendButton').addEventListener('click', submit);
+});
+
+const clear = function(ev) {
+    ev.preventDefault();
+
+    fetch('Php/clear.php', {
+        method: 'GET',
+    })
+        .then(res => res.text())
+        .then(table => document.querySelector('#res').innerHTML = table);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('clearButton').addEventListener('click', clear);
+});
---
+++ Php/function.php
@@ -0,0 +1,54 @@
+<?php
+function check($x, $y, $r)
+{
+    if (($x >= -$r && $x <= 0 && $y <= 0 && $y >=-$r) ||
+        ($r >= ($x - $y ) && $y <= 0 && $x >= 0) ||
+        (sqrt(($x * $x + $y * $y)) <= sqrt(($r * $r + $r * $r)) && $x <= 0 && $y >= 0)) {
+        return "<span style='color: green'>True</span>";
+    } else {
+        return "<span style='color: red'>False</span>";
+
+    }
+}
+
+function checkArea($x, $y, $r)
+{
+    return !in_array($y, array(-5, -4, -3, -2, -1, 0, 1, 2, 3))
+        || !is_numeric($x) || $x < -5 || $x > 5 ||
+        !in_array($r, array(1, 1.5, 2, 2.5, 3));
+}
+
+session_start();
+
+date_default_timezone_set('Europe/Moscow');
+$currentTime = date("H:i:s");
+$start = microtime(true);
+
+
+
+
+$x = (float)$_GET['x_val'];
+$y = (int)$_GET['y_val'];
+$r = (int)$_GET['r_val'];
+
+
+if (checkArea($x, $y, $r)) {
+    http_response_code(400);
+    return;
+}
+
+$res = check($x, $y, $r);
+$time = microtime(true) - $start;
+$result = array($x, $y, $r, $currentTime, $time, $res);
+
+//console.log("ready");
+
+
+if (!isset($_SESSION['history'])) {
+    $_SESSION['history'] = array();
+
+}
+
+array_push($_SESSION['history'], $result);
+
+include "table.php";
